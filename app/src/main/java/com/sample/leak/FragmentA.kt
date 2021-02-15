624f89052a312facf2817d745e17b8463baa2467
Christopher Elias <christopher.mike.96@gmail.com>
2021-02-15 16:28:23 -0500

    changes: onDestroy -> onDestroyView. Still leaks

--- app/src/main/java/com/sample/leak/FragmentA.kt
+++ app/src/main/java/com/sample/leak/FragmentA.kt
@@ -44,8 +44,8 @@ class FragmentA : Fragment() {
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 }
